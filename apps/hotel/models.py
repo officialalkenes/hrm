94cfb44e50ab198af62e18444337dbed1cb10806
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2023-01-23 12:20:52 +0100

    chore: room models and booking configured

--- apps/hotel/models.py
+++ apps/hotel/models.py
@@ -1,11 +1,42 @@
+from django.contrib.auth import get_user_model
+
 from django.db import models
 
 from django.utils.translation import gettext_lazy as _
 
-
-class Hotel(models.Model):
-    hotel_name = models.CharField(max_length=100, verbose_name=_("Hotel Name"))
+User = get_user_model()
 
 
-class Rooms(models.Model):
-    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, related_name=_("hotel"))
+class Room(models.Model):
+    room_name = models.CharField(max_length=255, unique=True)
+    room_number = models.CharField(max_length=255, unique=True)
+    room_type = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    capacity = models.PositiveIntegerField(verbose_name=_("Maximum People per room"))
+    beds = models.PositiveIntegerField(verbose_name=_("Number of Beds"))
+    image = models.ImageField(upload_to="rooms/")
+    description = models.TextField()
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.room_name
+
+
+class RoomImage(models.Model):
+    room = models.ForeignKey(
+        Room, on_delete=models.CASCADE, related_name=_("room_image")
+    )
+    image = models.ImageField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.room.room_name}"
+
+
+class Booking(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    customer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name=_("client")
+    )
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(max_length=255)
