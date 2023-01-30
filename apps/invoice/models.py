b9eca5c881c529cdd3b69a51b6d3cb035297c42c
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2023-01-30 13:35:34 +0100

    chore: create paystack payment models

--- apps/invoice/models.py
+++ apps/invoice/models.py
@@ -1,6 +1,29 @@
+import secrets
 from django.db import models
 
 # Create your models here.
+
+
+class Payment(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    ref = models.CharField(max_length=200)
+    email = models.EmailField()
+    verified = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self) -> str:
+        return f"Payment Ref: {self.ref}"
+
+    def save(self, *args, **kwargs):
+        while not self.ref:
+            ref = secrets.token_urlsafe(50)
+            object_with_same_ref = self.objects.filter(ref=ref)
+            if not object_with_same_ref:
+                self.ref = ref
+        return super().save(*args, **kwargs)
 
 
 # class HallType(models.Model):
