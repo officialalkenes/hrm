5074e624003d492a9e047c1d83075f00178e93f0
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2023-01-30 14:01:59 +0100

    chore: configured views and html template

--- apps/invoice/views.py
+++ apps/invoice/views.py
@@ -2,8 +2,16 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
+from apps.invoice.forms import PaymentForm
+
 
 # Create your views here.
 def initiate_payment(request: HttpResponse) -> HttpResponse:
     if request.method == "POST":
-        ...
+        payment = PaymentForm(request.POST)
+        if payment.is_valid():
+            payment = payment.save()
+            return render(request, "hotel/make-payment.html", {"payment": payment})
+    payment = PaymentForm()
+    context = {"payment": payment}
+    return render(request, "hotel/initiate-payment.html", context)
