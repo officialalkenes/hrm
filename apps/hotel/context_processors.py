32f34f253125adaa297a753c19ce03f45b11362c
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2023-02-24 00:17:46 +0100

    chore: update base templates and context processor

--- apps/hotel/context_processors.py
+++ apps/hotel/context_processors.py
@@ -6,7 +6,7 @@ from django.contrib.sites.shortcuts import get_current_site
 
 from django.db.models import Sum
 
-from .models import Room
+from .models import Room, RoomType
 
 User = get_user_model()
 
@@ -16,12 +16,14 @@ def dashboard_context(request):
     if request.user.is_authenticated:
         user = User.objects.all()
         all_rooms = Room.objects.all()
+        room_types = RoomType.objects.all()
         return {
             "paystack_public_key": settings.PAYSTACK_PUBLIC_KEY,
             "all_rooms": len(all_rooms),
             "total_guests": len(user),
             "available_rooms_count": len(all_rooms.filter(is_available=True)),
             "booked_rooms_count": len(all_rooms.filter(is_available=False)),
+            "room_types": room_types,
         }
     else:
         return {"": ""}
