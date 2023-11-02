7e3ae8ef83980827ac2c83fad9d0000fdc255c6f
officialalkenes <belloabdulhakeemolamide@gmail.com>
2023-11-02 11:54:38 +0100

    Chore: Revamped

--- core/settings.py
+++ core/settings.py
@@ -81,22 +81,22 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("NAME"),
-        # "HOST": config("HOST"),
-        "USER": config("USER"),
-        "PASSWORD": config("PASSWORD"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": config("NAME"),
+#         # "HOST": config("HOST"),
+#         "USER": config("USER"),
+#         "PASSWORD": config("PASSWORD"),
+#     }
+# }
 
 AUTH_USER_MODEL = "user.User"
 
