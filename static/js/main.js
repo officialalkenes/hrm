7fadd3acce6e5e0dbbeabd707a8abc82588e4d42
gideon-del <gideonchidi471@gmail.com>
2023-11-05 08:37:27 +0100

    refactor

--- static/js/main.js
+++ static/js/main.js
@@ -95,7 +95,7 @@
 
     /*------------------
 		Date Picker
-	--------------------*/
+	--------    ------------*/
     $(".date-input").datepicker({
         minDate: 0,
         dateFormat: 'dd MM, yy'
